613cb6f6cb5562aa41d455feae98e723764b048b
Kevin Clement <kevin.clement@gmail.com>
2019-10-06 14:12:08 -0700

    turn on coin

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ let audio = new (require('./audio'))({ logger: logger })
 let managers = [];
 // managers.push(new (require('./manager.bird'))({ name: 'bird', logger: logger, fb: fb }))
 // managers.push(new (require('./manager.cabinet'))({ name: 'cabinet', logger: logger, fb: fb }))
-managers.push(new (require('./manager.laser'))({ name: 'laser', logger: logger, fb: fb }))
-// managers.push(new (require('./manager.coin'))({ name: 'zoltar', logger: logger, fb: fb, audio: audio }))
+// managers.push(new (require('./manager.laser'))({ name: 'laser', logger: logger, fb: fb }))
+managers.push(new (require('./manager.coin'))({ name: 'zoltar', logger: logger, fb: fb, audio: audio }))
 // managers.push(new (require('./manager.hands'))({ name: 'hands', logger: logger, fb: fb }))
-managers.push(new (require('./manager.clock'))({ name: 'clock', logger: logger, fb: fb }))
+//managers.push(new (require('./manager.clock'))({ name: 'clock', logger: logger, fb: fb }))
 
 // might want to turn this off while doing dev, so I have a flag for it
 let ENABLE_FIREBASE_LOGS = true;
